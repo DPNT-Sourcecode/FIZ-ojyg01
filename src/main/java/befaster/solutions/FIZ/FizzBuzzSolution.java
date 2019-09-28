cd6e1b31c0079835b1c2d59e941f0fa95a173801
sbx_user1051 <sbx_user1051@169.254.140.197>
2019-09-28 11:13:40 +0000

    Sat Sep 28 11:13:40 UTC 2019

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -4,25 +4,56 @@ public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
 
-        Boolean fizz_flag = false,buzz_flag = false;
+        Boolean fizzFlag = false, buzzFlag = false, deluxeFlag = false;
+        Integer nrTrueFlags = 0;
+        String stringToBeReturned = "";
 
-       if (number % 3 == 0 || number.toString().contains("3")) {
-           fizz_flag = true;
-       }
-        if (number % 5 == 0 || number.toString().contains("5")) {
-            buzz_flag = true;
+        if (number % 3 == 0 || number.toString().contains("3")) {
+            fizzFlag = true;
+            nrTrueFlags++;
         }
 
-        if (fizz_flag.equals(true) && buzz_flag.equals(true))
-            return "fizz buzz";
-        else if (fizz_flag.equals(true))
-            return "fizz";
-        else if (buzz_flag.equals(true))
-            return "buzz";
-        else return number.toString();
+        if (number % 5 == 0 || number.toString().contains("5")) {
+            buzzFlag = true;
+            nrTrueFlags++;
+        }
 
+        if (number > 10) {
+            Integer tempNumber = number;
+            Integer lastDigitOfTheNumber = number % 10;
+            while (tempNumber % 10 == lastDigitOfTheNumber) {
+                tempNumber = tempNumber / 10;
+                if (tempNumber > 0 == false) {
+                    deluxeFlag = true;
+                    nrTrueFlags++;
+                    break;
+                }
+            }
+        }
+
+        if (fizzFlag.equals(true)) {
+            stringToBeReturned = "fizz";
+        }
+        if (buzzFlag.equals(true)) {
+            if (nrTrueFlags > 1) {
+                stringToBeReturned = stringToBeReturned + " buzz";
+            } else {
+                stringToBeReturned = "buzz";
+            }
+        }
+        if (deluxeFlag.equals(true)) {
+            if (nrTrueFlags > 1) {
+                stringToBeReturned = stringToBeReturned + " deluxe";
+            } else {
+                stringToBeReturned = "deluxe";
+            }
+        }
+
+        if (stringToBeReturned.isEmpty() == false)
+            return stringToBeReturned;
+        return number.toString();
     }
 
-  
 }
+
 
