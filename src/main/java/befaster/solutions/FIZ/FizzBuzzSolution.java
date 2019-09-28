22e6329b5e315644dfa4e9fbd35a6644b4f17ad3
sbx_user1051 <sbx_user1051@169.254.140.197>
2019-09-28 10:55:43 +0000

    Sat Sep 28 10:55:43 UTC 2019

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -3,19 +3,26 @@ package befaster.solutions.FIZ;
 public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
-        if (number % 3 == 0 && number % 5 == 0)
+
+        Boolean fizz_flag = false,buzz_flag = false;
+
+       if (number % 3 == 0 || number.toString().contains("3")) {
+           fizz_flag = true;
+       }
+        if (number % 5 == 0 || number.toString().contains("5")) {
+            buzz_flag = true;
+        }
+
+        if (fizz_flag.equals(true) && buzz_flag.equals(true))
             return "fizz buzz";
-        else if (number % 3 == 0) {
+        else if (fizz_flag.equals(true))
             return "fizz";
-        } else if (number % 5 == 0) {
+        else if (buzz_flag.equals(true))
             return "buzz";
-        } else
-            return number.toString();
-    }
-
-    public static void main(String[] args) {
-        FizzBuzzSolution sol = new FizzBuzzSolution();
-        System.out.println(sol.fizzBuzz(4));
+        else return number.toString();
 
     }
+
+  
 }
+
